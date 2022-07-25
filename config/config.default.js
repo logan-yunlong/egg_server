606321dd41030249ea206e1c96312fc2308b9734
loganylwu <loganylwu@tencent.com>
2022-07-25 15:21:55 +0800

    [+] add base page

--- config/config.default.js
+++ config/config.default.js
@@ -4,6 +4,12 @@ const appInfo = (appInfo) => {
     const config = {};
     config.keys = appInfo.name + '123678142_1238917';
     // config.middleware = ['cors'];
+    config.view = {
+        defaultViewEngine: 'nunjucks',
+        mapping: {
+            '.tpl': 'nunjucks'
+        }
+    }
     return config;
 };
 
