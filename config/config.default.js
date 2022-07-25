04b72dfa9fa9019f7091b87dc8cd8e4e519dec59
loganylwu <loganylwu@tencent.com>
2022-07-25 15:14:39 +0800

    [+] add base controller

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const appInfo = (appInfo) => {
+    const config = {};
+    config.keys = appInfo.name + '123678142_1238917';
+    // config.middleware = ['cors'];
+    return config;
+};
+
+module.exports = appInfo;
